b742a129ea34c758ad6b276a35fdd922579f3d3d
Lishitingxdl <1289873071@qq.com>
2018-04-03 08:26:51 +0800

    1

--- js/tmall.js
+++ js/tmall.js
@@ -32,3 +32,52 @@
   		}
   	})
   })
+//menu
+{
+    $(".banner_nav1")
+        .mouseenter(function () {
+            let index = $(this).index(".banner_nav1");
+            $(".menu").eq(index).show();
+        })
+        .mouseleave(function () {
+            let index = $(this).index(".banner_nav1");
+            $(".menu").eq(index).hide();
+        })
+}
+//banner
+  {
+    $(".banner_pager li").mouseenter(function () {
+        let index = $(this).index(".banner_pager li");
+        $(".banner_pager li").removeClass("active").eq(index).addClass("active");
+        $(".img").removeClass("active").eq(index).addClass("active");
+        n = index;
+    })
+
+    //自动轮播
+    let n = 0;
+    let l = $(".img").length;
+
+    let st=setInterval(move, 3000);
+
+    function move() {
+        n++;
+        if (n === l) {
+            n = 0;
+        }
+        if (n < 0) {
+            n = l - 1;
+        }
+        $(".banner_pager li").removeClass("active").eq(n).addClass("active");
+        $(".img").removeClass("active").eq(n).addClass("active");
+        switch (n){
+            case "1":$(".banner").removeClass("")
+        }
+    }
+
+    $(".bannerimg").mouseenter(function () {
+        clearInterval(st)
+    })
+    $(".bannerimg").mouseleave(function () {
+        st=setInterval(move,3000);
+    })
+}
